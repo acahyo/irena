5129b0dc99953a2d9f97a972f474988491a26fc2
andika cahyo <acahyo310940@gmail.com>
2025-09-30 06:25:27 +0000

    Error: useMemo is not defined

--- src/app/dashboard/hse/new/page.tsx
+++ src/app/dashboard/hse/new/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import Link from 'next/link';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { format } from 'date-fns';
